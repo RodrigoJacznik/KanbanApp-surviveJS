c4c3671847ebd0a9067f8965cc1cef8f1f3e49b1
Rodrigo Jacznik <r.jacznik@globallogic.com>
2015-11-04 17:00:42 -0300

    Add %Flux% architecture with alt

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,81 +1,27 @@
-import uuid from 'node-uuid'
 import React from 'react'
 import Notes from './Notes.jsx'
+import NoteActions from '../actions/NoteActions.js'
+import NoteStore from '../stores/NoteStore.js'
 
 
 class App extends React.Component {
     constructor (props) {
         super(props)
 
-        this.state = {
-            notes: [
-                {
-                    id: uuid.v4(),
-                    task: 'Learn Webpack'
-                },
-                {
-                    id: uuid.v4(),
-                    task: 'Learn React'
-                },
-                {
-                    id: uuid.v4(),
-                    task: 'Do laundry'
-                }
-            ]
-        }
-
-        this.findNote = this.findNote.bind(this)
-        this.editNote = this.editNote.bind(this)
-        this.addNote = this.addNote.bind(this)
-        this.deleteNote = this.deleteNote.bind(this)
+        this.storeChanged = this.storeChanged.bind(this)
+        this.state = NoteStore.getState()
     }
 
-    editNote (id, task) {
-        let notes = this.state.notes
-        const noteIndex = this.findNote(id)
-
-        if (noteIndex < 0) {
-            return
-        }
-
-        notes[noteIndex].task = task
-
-        this.setState({notes})
+    componentDidMount() {
+        NoteStore.listen(this.storeChanged)
     }
 
-    findNote (id) {
-        const notes = this.state.notes
-        const noteIndex = notes.findIndex((note) => note.id === id)
-
-        if (noteIndex < 0) {
-            console.warn("Failed to find note", notes, id)
-        }
-
-        return noteIndex
+    componentWillUnmount() {
+        NoteStore.unlisten(this.storeChanged)
     }
 
-    addNote () {
-        var newTask = {
-            id: uuid.v4(),
-            task: 'New task'
-        }
-
-        this.setState({
-            notes: this.state.notes.concat([newTask])
-        })
-    }
-
-    deleteNote (id) {
-        const notes = this.state.notes
-        const noteIndex = this.findNote(id)
-
-        if (noteIndex < 0) {
-            return
-        }
-
-        this.setState({
-            notes: notes.slice(0, noteIndex).concat(notes.slice(noteIndex + 1))
-        })
+    storeChanged(state) {
+        this.setState(state)
     }
 
     render () {
@@ -84,9 +30,22 @@ class App extends React.Component {
         return (
             <div>
                 <button className="add-note" onClick={this.addNote}>+</button>
-                <Notes items={notes} onEdit={this.editNote} onDelete={this.deleteNote} />
+                <Notes items={notes}
+                    onEdit={this.editNote} onDelete={this.deleteNote} />
             </div>
         )
+    }
+
+    addNote() {
+        NoteActions.create({task: "New task"})
+    }
+
+    editNote(id, task) {
+        NoteActions.update({id, task})
+    }
+
+    deleteNote(id) {
+        NoteActions.delete(id)
     }
 }
 
