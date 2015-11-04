4b283274051b2a8c3eb67129c64f8bc03c71f232
Rodrigo Jacznik <jacznik.rodrigo@gmail.com>
2015-11-04 17:23:49 -0300

    Add alt-container

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,3 +1,4 @@
+import AltContainer from 'alt-container'
 import React from 'react'
 import Notes from './Notes.jsx'
 import NoteActions from '../actions/NoteActions.js'
@@ -5,33 +6,15 @@ import NoteStore from '../stores/NoteStore.js'
 
 
 class App extends React.Component {
-    constructor (props) {
-        super(props)
-
-        this.storeChanged = this.storeChanged.bind(this)
-        this.state = NoteStore.getState()
-    }
-
-    componentDidMount() {
-        NoteStore.listen(this.storeChanged)
-    }
-
-    componentWillUnmount() {
-        NoteStore.unlisten(this.storeChanged)
-    }
-
-    storeChanged(state) {
-        this.setState(state)
-    }
-
     render () {
-        const notes = this.state.notes
-
         return (
             <div>
                 <button className="add-note" onClick={this.addNote}>+</button>
-                <Notes items={notes}
-                    onEdit={this.editNote} onDelete={this.deleteNote} />
+                <AltContainer stores={[NoteStore]}
+                        inject={{ items: () => NoteStore.getState().notes }}>
+
+                    <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+                </AltContainer>
             </div>
         )
     }
